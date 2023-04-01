5debc59f4ee50f253260b7fbea2754c63cce69cc
palewire <b@palewi.re>
2023-04-01 15:53:07 -0400

    No dtype

--- newshomepages/site.py
+++ newshomepages/site.py
@@ -182,12 +182,6 @@ def accessibility_ranking():
             "accessibility_color",
             "accessibility_rank",
         ],
-        dtype={
-            "handle": str,
-            "accessibility_median": float,
-            "accessibility_color": str,
-            "accessibility_rank": float,
-        },
     )
 
     # Calculate the grand total
@@ -244,12 +238,6 @@ def performance_ranking():
             "performance_color",
             "performance_rank",
         ],
-        dtype={
-            "handle": str,
-            "performance_median": float,
-            "performance_color": str,
-            "performance_rank": float,
-        },
     )
 
     # Calculate the grand total
