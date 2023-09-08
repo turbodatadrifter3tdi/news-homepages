dca9251742dbe0abd9e471ce44f64dd2fb576e16
palewire <b@palewi.re>
2023-09-07 20:40:07 -0400

    A couple moves

--- tests/test_robotstxt.py
+++ tests/test_robotstxt.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from click.testing import CliRunner
 
 from newshomepages import robotstxt
@@ -8,3 +10,11 @@ def test_robotstxt_cli(tmp_path):
     runner = CliRunner()
     result = runner.invoke(robotstxt.cli, ["latimes", "-o", tmp_path])
     assert result.exit_code == 0
+
+
+def test_sqlite_extensions():
+    """Verify that the sqlite extension can be loaded."""
+    conn = sqlite3.connect(":memory:")
+    conn.enable_load_extension(True)
+    conn.load_extension("libsqlite_robotstxt")
+    conn.close()
