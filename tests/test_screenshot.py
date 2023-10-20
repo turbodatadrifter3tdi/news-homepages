ba82ecd28d08f88f12fa73a798ef9dc9c467fbe7
palewire <b@palewi.re>
2023-10-20 14:52:13 -0400

    Rework tests

--- tests/test_screenshot.py
+++ tests/test_screenshot.py
@@ -6,12 +6,12 @@ from newshomepages import screenshot
 def test_cropped(tmp_path):
     """Test a cropped screenshot."""
     runner = CliRunner()
-    result = runner.invoke(screenshot.cli, ["latimes", "-o", tmp_path])
+    result = runner.invoke(screenshot.cli, ["drudge", "-o", tmp_path])
     assert result.exit_code == 0
 
 
 def test_full(tmp_path):
     """Test a full-page screenshot."""
     runner = CliRunner()
-    result = runner.invoke(screenshot.cli, ["latimes", "-o", tmp_path, "--full-page"])
+    result = runner.invoke(screenshot.cli, ["drudge", "-o", tmp_path, "--full-page"])
     assert result.exit_code == 0
